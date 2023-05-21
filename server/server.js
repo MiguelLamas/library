5f87e1ddfb9b36c7b9260abc992af8e82c0c27d7
Miguel Lamas <lamasmigs@yahoo.co.uk>
2023-05-21 21:47:16 +0100

    Configured Node Static Service & Deploying Node on Render

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // CONTROLLERS
 const booksController = require('./controllers/books_controller');
@@ -21,4 +22,8 @@ app.use('/api/books', booksController);
 // LISTEN
 app.listen(4005, () => {
     console.log('Server is running on port 4005');
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 })
